174bab00e6c0edf23d7825da03ae5618f6398b50
Ethan <ethantmanns@gmail.com>
2020-04-12 19:16:43 -0700

    Replace hydrate function with render to avoid css dropping bug in gastby/react

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,12 @@
 import "typeface-source-sans-pro"
 
 import "prismjs/themes/prism.css"
+
+// replace hydrate function with straight render to fix https://github.com/gatsbyjs/gatsby/issues/8560. Does affect performance
+const ReactDOM = require("react-dom")
+
+export function replaceHydrateFunction() {
+  return (element, container, callback) => {
+    ReactDOM.render(element, container, callback)
+  }
+}
