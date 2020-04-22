e498c9e2e521bdfc927a20efb1521f4b040d4414
Ethan Manns <ethantmanns@gmail.com>
2020-04-21 18:49:05 -0700

    Add homepage content (#13)

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,21 +6,43 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import yellowLogo from "../../content/assets/yellow-logo.png"
 
-const home = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
+const home = ({ location }) => {
   const content = (
     <div className="body">
-      <h1>{siteTitle}</h1>
-      <p>
-        Website coming soon! In the meantime, join us using{" "}
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfc06mx_j5Adhc5xDdWZjZI7turnaZOH7Q6sndllJKq4i678w/viewform">
-          this form
-        </a>
-        !
-      </p>
-      <image src={yellowLogo} />
-      <img src={yellowLogo} alt="" className="big-home-image" />
+      <h1>WELCOME TO SUNRISE SEATTLE!</h1>
+      <div className="section">
+        <h2>What's Sunrise?</h2>
+        <p className="section-text">
+          The Sunrise Movement is a national, youth-led movement that aims to
+          stop the climate crisis, create millions of good jobs, and take action
+          against the systems of oppression at the root of the climate crisis.
+          We are ordinary young people who are scared about what the climate
+          crisis means for the people and places we love. Together, we will
+          change this country and this world, sure as the sun rises each
+          morning.
+        </p>
+      </div>
+
+      <div className="section-embedded">
+        <div className="calendar">
+          <iframe
+            src="https://calendar.google.com/calendar/embed?src=d4a11ua9ap43jtl6h0rhngsf8s%40group.calendar.google.com&ctz=America%2FLos_Angeles"
+            width="600"
+            height="480"
+            frameborder="0"
+            scrolling="no"
+          ></iframe>
+        </div>
+        <div className="section-vertical">
+          <h2>Upcoming Events</h2>
+          <p>
+            We rely on the participation of our members to succeed. When
+            everybody actively plugs in and gives what they can (whether you can
+            give 5 minutes a week or 40 hours), our movement thrives! To get
+            plugged in, join us at one of our upcoming events we have planned.
+          </p>
+        </div>
+      </div>
     </div>
   )
   return (
