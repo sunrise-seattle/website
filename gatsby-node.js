fe41c0b1977880b03bb0ca18170edf3946726a30
Ethan Manns <ethantmanns@gmail.com>
2020-05-19 08:47:42 -0700

    Add redirect for people's alignment training form

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,4 +8,10 @@ exports.createPages = async ({ actions }) => {
       "https://docs.google.com/forms/d/e/1FAIpQLSd9pgewT90daFO_8duMxoB4CcnBIm24pwd0wlM2PSf9hXDE2w/viewform?usp=sf_link",
     isPermanent: false,
   })
+  createRedirect({
+    fromPath: "/peoples-alignment",
+    toPath:
+      "https://docs.google.com/forms/d/e/1FAIpQLSdbKDv0IB_Hg7nbuItELCewk3D3d9wBS9MDTsk2FwhZPL2u3w/viewform",
+    isPermanent: false,
+  })
 }
