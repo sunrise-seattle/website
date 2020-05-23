f41867e8a8f033e6e2a00ed93145bf0cfb88c24b
Ethan <ethantmanns@gmail.com>
2020-05-23 15:35:43 -0700

    Add stylized navbar

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,26 @@
-import React from "react"
-import { graphql } from "gatsby"
-
 import Layout from "../components/layout"
+import React from "react"
 import SEO from "../components/seo"
+import { graphql } from "gatsby"
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="404: Not Found" />
+  const content = (
+    <>
       <h1>Not Found</h1>
-      <p>Page not found</p>
+      <p>
+        The page you were looking for was not found. This site is still under
+        construction, so it could be that the page hasn't been created yet, or
+        it could have been moved.
+      </p>
+      <a href="/">Return home</a>
+    </>
+  )
+
+  return (
+    <Layout location={location} title={siteTitle} content={content}>
+      <SEO title="404: Not Found" />
     </Layout>
   )
 }
