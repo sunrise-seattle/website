4549f206ca3b043a860eb1d806639543b790ccdc
Ethan Manns <ethantmanns@gmail.com>
2020-06-04 10:09:01 -0700

    Add black lives redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,15 +3,9 @@ exports.createPages = async ({ actions }) => {
 
   // create redirects
   createRedirect({
-    fromPath: "/bailout",
+    fromPath: "/blacklives",
     toPath:
-      "https://docs.google.com/forms/d/e/1FAIpQLSd9pgewT90daFO_8duMxoB4CcnBIm24pwd0wlM2PSf9hXDE2w/viewform?usp=sf_link",
-    isPermanent: false,
-  })
-  createRedirect({
-    fromPath: "/peoples-alignment",
-    toPath:
-      "https://docs.google.com/forms/d/e/1FAIpQLSdbKDv0IB_Hg7nbuItELCewk3D3d9wBS9MDTsk2FwhZPL2u3w/viewform",
+      "https://docs.google.com/document/d/1si7VhtmmXTD9aVONniEIVP5Doa3DRQvGfcx5CfMlaSA",
     isPermanent: false,
   })
 }
