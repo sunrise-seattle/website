1e97d4817f374bc1365b35af8e239d34c4f0f4eb
Ethan Manns <ethantmanns@gmail.com>
2020-08-14 14:20:52 -0700

    Add mutual aid form redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,4 +8,10 @@ exports.createPages = async ({ actions }) => {
       "https://docs.google.com/document/d/1si7VhtmmXTD9aVONniEIVP5Doa3DRQvGfcx5CfMlaSA",
     isPermanent: false,
   })
+  createRedirect({
+    fromPath: "/mutualaid",
+    toPath:
+      "https://docs.google.com/document/d/1psyAYkZuipqr2Nh2RfafCTeLFVnaUdNB3w_G9NH7Goc",
+    isPermanent: false,
+  })
 }
